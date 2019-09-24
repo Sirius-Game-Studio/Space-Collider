a6fdc98d67b09db9b8353343b2f2bc7949217411
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-24 20:46:01 +0600

    Improved code

--- Space Collider (Windows)/Assets/Scripts/Core Scripts/BulletHit.cs
+++ Space Collider (Windows)/Assets/Scripts/Core Scripts/BulletHit.cs
@@ -5,38 +5,42 @@ public class BulletHit : MonoBehaviour
     public long damage = 1;
     [SerializeField] private GameObject explosion = null;
 
+    private bool destroy = false;
+
     void Update()
     {
+        if (destroy)
+        {
+            if (explosion)
+            {
+                GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
+                if (newExplosion.GetComponent<AudioSource>()) newExplosion.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundVolume");
+            }
+            Destroy(gameObject);
+        }
         if (damage < 1) damage = 1; //Checks if damage is below 1
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Enemy"))
+        if (!destroy)
         {
-            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
-            if (enemyHealth)
+            if (other.CompareTag("Enemy"))
             {
-                enemyHealth.takeDamage(damage);
-                if (explosion)
+                EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+                if (enemyHealth)
                 {
-                    GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
-                    if (newExplosion.GetComponent<AudioSource>()) newExplosion.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundVolume");
+                    enemyHealth.takeDamage(damage);
+                    destroy = true;
                 }
-                Destroy(gameObject);
-            }
-        } else if (other.CompareTag("Shield"))
-        {
-            ShieldHealth shieldHealth = other.GetComponent<ShieldHealth>();
-            if (shieldHealth)
+            } else if (other.CompareTag("Shield"))
             {
-                shieldHealth.takeDamage(damage);
-                if (explosion)
+                ShieldHealth shieldHealth = other.GetComponent<ShieldHealth>();
+                if (shieldHealth)
                 {
-                    GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
-                    if (newExplosion.GetComponent<AudioSource>()) newExplosion.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundVolume");
+                    shieldHealth.takeDamage(damage);
+                    destroy = true;
                 }
-                Destroy(gameObject);
             }
         }
     }
