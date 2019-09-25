6b3c9607703093c013b95fa40dd7a68e03a4ca79
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-25 13:28:14 +0600

    Update EnemyBulletHit.cs

--- Space Collider (Windows)/Assets/Scripts/Enemy/EnemyBulletHit.cs
+++ Space Collider (Windows)/Assets/Scripts/Enemy/EnemyBulletHit.cs
@@ -2,16 +2,13 @@
 
 public class EnemyBulletHit : MonoBehaviour
 {
-    [Tooltip("The amount of damage dealt to the player's Ship Armor.")] public long damage = 1;
-    [Tooltip("Damage multiplier on Hard mode (only applies to Survival Mode).")] public float hardDamageMultiplier = 1;
+    [Tooltip("How much damage this projectile deals to the player's Ship Armor.")] public long damage = 1;
+    [Tooltip("Damage multiplier on Hard mode (Survival only).")] public float hardDamageMultiplier = 1;
     [SerializeField] private GameObject explosion = null;
-
-    private GameController gameController;
 
     void Start()
     {
-        gameController = FindObjectOfType<GameController>();
-        if (!gameController.isStandard && gameController.difficulty >= 3) damage = (long)(damage * hardDamageMultiplier);
+        if (!GameController.instance.isCampaign && GameController.instance.difficulty >= 3) damage = (long)(damage * hardDamageMultiplier);
     }
 
     void Update()
@@ -21,30 +18,19 @@ public class EnemyBulletHit : MonoBehaviour
 
     void OnTriggerStay(Collider other)
     {
-        if (!gameController.won)
+        if (!GameController.instance.gameOver)
         {
             PlayerController playerController = other.GetComponent<PlayerController>();
             ShieldHealth shieldHealth = other.GetComponent<ShieldHealth>();
-            if (other.CompareTag("Player") && playerController)
+            if (other.CompareTag("Player") && playerController && !playerController.invulnerable)
             {
-                if (!playerController.invulnerable)
-                {
-                    playerController.onHit(damage, false);
-                    if (explosion)
-                    {
-                        GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
-                        if (newExplosion.GetComponent<AudioSource>()) newExplosion.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundVolume");
-                    }
-                    Destroy(gameObject);
-                }
+                playerController.onHit(damage, false);
+                if (explosion) Instantiate(explosion, transform.position, transform.rotation);
+                Destroy(gameObject);
             } else if (other.CompareTag("Shield") && shieldHealth)
             {
-                if (explosion)
-                {
-                    GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
-                    if (newExplosion.GetComponent<AudioSource>()) newExplosion.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundVolume");
-                }
                 shieldHealth.takeDamage(damage);
+                if (explosion) Instantiate(explosion, transform.position, transform.rotation);
                 Destroy(gameObject);
             }
         }
