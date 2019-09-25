e3737f64fa52564bd35d613874e27493748398a8
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-25 13:28:57 +0600

    Update BulletHit.cs

--- Space Collider (Windows)/Assets/Scripts/Player/BulletHit.cs
+++ Space Collider (Windows)/Assets/Scripts/Player/BulletHit.cs
@@ -5,43 +5,25 @@ public class BulletHit : MonoBehaviour
     public long damage = 1;
     [SerializeField] private GameObject explosion = null;
 
-    private bool destroy = false;
-
     void Update()
     {
-        if (destroy)
-        {
-            if (explosion)
-            {
-                GameObject newExplosion = Instantiate(explosion, transform.position, transform.rotation);
-                if (newExplosion.GetComponent<AudioSource>()) newExplosion.GetComponent<AudioSource>().volume = PlayerPrefs.GetFloat("SoundVolume");
-            }
-            Destroy(gameObject);
-        }
         if (damage < 1) damage = 1; //Checks if damage is below 1
     }
 
     void OnTriggerStay(Collider other)
     {
-        if (!destroy)
+        EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+        ShieldHealth shieldHealth = other.GetComponent<ShieldHealth>();
+        if (other.CompareTag("Enemy") && enemyHealth)
         {
-            if (other.CompareTag("Enemy"))
-            {
-                EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
-                if (enemyHealth)
-                {
-                    enemyHealth.takeDamage(damage);
-                    destroy = true;
-                }
-            } else if (other.CompareTag("Shield"))
-            {
-                ShieldHealth shieldHealth = other.GetComponent<ShieldHealth>();
-                if (shieldHealth)
-                {
-                    shieldHealth.takeDamage(damage);
-                    destroy = true;
-                }
-            }
+            enemyHealth.takeDamage(damage);
+            if (explosion) Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
+        } else if (other.CompareTag("Shield"))
+        {
+            shieldHealth.takeDamage(damage);
+            if (explosion) Instantiate(explosion, transform.position, transform.rotation);
+            Destroy(gameObject);
         }
     }
 }
