4cd5872a22664fb0abdab1d13bb714c9680ea39b
Roopkotha Studio <roopkotha7d@gmail.com>
2019-09-30 20:11:05 +0600

    Created ControllerMusicAdjuster script

---
+++ Space Collider (Windows)/Assets/Scripts/Core/ControllerMusicAdjuster.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class ControllerMusicAdjuster : MonoBehaviour
+{
+    [SerializeField] private SetVolume volumeSlider = null;
+
+    private Controls input;
+
+    void Awake()
+    {
+        input = new Controls();
+    }
+
+    void OnEnable()
+    {
+        input.Enable();
+        input.Sound.LowerMusic.performed += context => volumeSlider.controllerAdjust(false, true);
+        input.Sound.IncreaseMusic.performed += context => volumeSlider.controllerAdjust(true, false);
+        input.Sound.LowerMusic.canceled += context => volumeSlider.controllerCancel();
+        input.Sound.IncreaseMusic.canceled += context => volumeSlider.controllerCancel();
+    }
+
+    void OnDisable()
+    {
+        input.Disable();
+        input.Sound.LowerMusic.performed -= context => volumeSlider.controllerAdjust(false, true);
+        input.Sound.IncreaseMusic.performed -= context => volumeSlider.controllerAdjust(true, false);
+        input.Sound.LowerMusic.canceled -= context => volumeSlider.controllerCancel();
+        input.Sound.IncreaseMusic.canceled -= context => volumeSlider.controllerCancel();
+    }
+}
